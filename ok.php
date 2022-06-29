27c8e7f8aa585aad5e47f542cf958e0fbad82a2b
iaskeyz69 <iaskeyz.09@gmail.com>
2022-06-29 22:09:33 +0800

    Change html file to php

---
+++ ok.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Git Demo Page </h1>
+    <h2>Hello again</h2>
+      <script src="app.js"></script>
+
+    <form action="try.php" method="post">
+        <p>Enter Your Name: </p>
+        <input type="text" id="name" name="name"> <br><br>
+        <p>Enter your ID: </p>
+        <input type="text" id="name" name="name"> <br> <br>
+        <input type="submit" value="Click Me">  
+    </form>
+</body>
+</html>
