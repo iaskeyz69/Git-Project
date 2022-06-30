ba8dfa44fb0895c8f55d6f7db8da52c9670fff37
iaskeyz69 <iaskeyz.09@gmail.com>
2022-06-30 13:59:33 +0800

    -

--- ok.php
+++ ok.php
@@ -17,7 +17,12 @@
       <input type="text" name="num2" id="num2"> <br> <br>
       <input type="submit" value="add"> <br>
       <script src="app.js"></script>
-      
+      <?php 
+      $num1 = $_POST["num1"];
+      $num2 = $_POST["num2"];
+      $result = $num1 + $num2;
+
+      echo "the result is: $result";
     ?> 
     </fieldset>
     </form>
@@ -28,28 +33,7 @@
             <th>Number 2</th>
             <th>Result</th>
         </tr>
-        <?php 
-    $conn = mysqli_connect("localhost:3306 ", "julias2", "julias_123*", "table_db");
-    if ($conn -> connect_error) {
-        die("Connection Failed: ". $conn -> connect_error);
-
-    }
-
-    $sql = "SELECT no, isbn, title, author, price, category from book";
-    $result = $conn -> query($sql);
-
-    if ($result -> num_rows > 0) {
-            while ($row = $result -> fetch_assoc()) {
-                echo "<tr><td>". $row["num1"]. "</td><td>". $row["num2"]. "</td><td>"
-                . $row["result"]."</td></tr>";
-            }
-            echo "</table>";
-    }
-    else {
-        echo  "0 result";
-    }
-    $conn -> close();
-    ?>
+    
     </table>
    
     
