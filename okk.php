8747470f8dc42ee3252ea97f718644da66672162
iaskeyz69 <iaskeyz.09@gmail.com>
2022-06-30 14:10:20 +0800

    test

--- okk.php
+++ okk.php
@@ -5,7 +5,7 @@ $result = $num1 + $num2;
 
 echo "the result is: $result";
 
-$conn = mysqli_connect("localhost", "root", "", "table_db");
+$conn = mysqli_connect("localhost:3306 ", "julias2", "julias_123*", "table_db");
 if($conn -> connect_error) {
     die("Connection Failed:". $conn -> connect_error);
 }
@@ -13,7 +13,7 @@ if($conn -> connect_error) {
 else {
     $stmt = $conn ->prepare("Insert into number_digits(num1, num2, result) values(? ,? ,? )");
     
-    $stmt -> bind_param("iii", $num1, $num2, $result);
+    $stmt -> bind_param("sss", $num1, $num2, $result);
     $stmt -> execute();
 
     echo "User Input Successfully";
